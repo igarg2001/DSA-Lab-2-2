15b218999ddfb4b99a86b6674afa159a2620da9b
igarg2001 <igarg2001@gmail.com>
2021-01-31 12:43:33 +0530

    initialize head->count to 0

--- Lab2/driver.c
+++ Lab2/driver.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include "stack.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     FILE *fptr1 = fopen("stackInput.txt", "r");
     FILE *fptr2 = fopen("stackOutput.txt", "w");
-    struct linkedList *ll = (struct linkedList *) malloc(sizeof(struct linkedList));
+    struct linkedList *ll = (struct linkedList *)malloc(sizeof(struct linkedList));
+    ll->count = 0;
     struct Stack *stk = (struct Stack *)malloc(sizeof(struct Stack));
     stk->l = ll;
     //pushing into stack
-    while(!feof(fptr1)) {
+    while (!feof(fptr1))
+    {
         int temp;
         fscanf(fptr1, "%d", &temp);
         push(stk, temp);
@@ -16,12 +19,13 @@ int main(int argc, char *argv[]) {
     fclose(fptr1);
     //popping from stack
     struct node *curr;
-    while((curr=pop(stk))!=NULL) {
+    while ((curr = pop(stk)) != NULL)
+    {
         //printing to file
         fprintf(fptr2, "%d\n", curr->element);
         //printing to stdout
         printf("%d ", curr->element);
-        curr=curr->next;
+        curr = curr->next;
     }
     fclose(fptr2);
 }
